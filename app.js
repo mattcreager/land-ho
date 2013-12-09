c78b3430fc004e2bd89ffb7f55854237ea08d7b5
Matt Creager <mattcreager@goinstant.com>
2013-12-09 15:02:01 -0400

    Inital Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/* jshint node: true */
+
+'use strict';
+
+var landho = require('./app/server');
+
+landho.start();
---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+
+/* global goinstant */
+
+'use strict';
+
+/* Dependencies */
+
+var giStatus = require('./gi_status');
+
+var url = 'https://goinstant.net/mattcreager/land-ho';
+var platform = new goinstant.Platform(url);
+
+platform.connect(function (err) {
+  if (err) {
+    giStatus.connected(false);
+    console.log('Error connecting to platform:', err);
+    return;
+  }
+
+  giStatus.connected(true);
+});
